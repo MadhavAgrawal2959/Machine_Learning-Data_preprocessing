aa704591376a59f6cc540202457744bf81ef7bef
Madhav Agrawal <117247645+MadhavAgrawal2959@users.noreply.github.com>
2024-02-11 01:19:36 +0530

    Initial commit

---
+++ Data_preprocessing.py
@@ -0,0 +1,68 @@
+
+## Importing the library.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+## Importing the dataset Data.csv.
+
+dataset= pd.read_csv("Data.csv")
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+
+## printing the matrix of features and printing the matrix of predictors
+
+print(X)
+
+print(y)
+
+##Taking care of missing data
+
+from sklearn.impute import SimpleImputer
+imputer=SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+
+print(X)
+
+##Encoding the Independent Variable
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+X=ct.fit_transform(X)
+X=np.array(X)
+
+print(X)
+
+##Encoding the Dependent Variable
+
+from sklearn.preprocessing import LabelEncoder
+le= LabelEncoder()
+y=le.fit_transform(y)
+
+print(y)
+
+##Splitting the dataset
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1)
+
+print(X_train)
+
+print(X_test)
+
+print(y_train)
+
+print(y_test)
+
+##Feature scaling on the dataset
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+X_train[:,3:]=scaler.fit_transform(X_train[:,3:])
+X_test[:,3:]=scaler.transform(X_test[:,3:])
+
+print(X_train)
+
+print(X_test)
